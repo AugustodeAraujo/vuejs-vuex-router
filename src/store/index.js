4a902bf0d1c3fc37db1d278691cc95bbc42996ac
AugustodeAraujo <poxaugustodantas@gmail.com>
2021-09-20 21:05:42 -0300

    Vuex & Vue Router Studies

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,40 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
+    products: [],
+    productsInBag: [],
   },
   mutations: {
+    loadProducts(state, products) {
+      console.log(products)
+      state.products = products
+    },
+    addToBag(state, product) {
+      state.productsInBag.push(product)
+    },
+    removeFromBag(state, productId) {
+      const updatedBag = state.productsInBag.filter(
+        item => productId != item.id
+      )
+      state.productsInBag = updatedBag
+    },
   },
   actions: {
+    loadProducts({ commit }) {
+      axios.get('https://fakestoreapi.com/products').then(response => {
+        commit('loadProducts', response.data)
+      })
+    },
+    addToBag({ commit }, product) {
+      commit('addToBag', product)
+    },
+    removeFromBag({ commit }, productId) {
+      if (confirm('Quer remover do carrinho?')) {
+        commit('removeFromBag', productId)
+      }
+    },
   },
-  modules: {
-  }
+  modules: {},
 })
